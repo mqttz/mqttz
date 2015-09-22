ed97a3b2f1af9b7374116d8fc5c37e066dd32288
Roger A. Light <roger@atchoo.org>
2015-09-22 14:08:10 +0100

    Fix potential memory leaks.

--- src/context.c
+++ src/context.c
@@ -116,20 +116,25 @@ void mqtt3_context_cleanup(struct mosquitto_db *db, struct mosquitto *context, b
 			_mosquitto_free(context->bridge->local_clientid);
 			context->bridge->local_clientid = NULL;
 		}
-		if(context->bridge->remote_username){
-			context->bridge->remote_username = NULL;
-		}
-		if(context->bridge->remote_password){
-			context->bridge->remote_password = NULL;
-		}
 		if(context->bridge->local_username){
+			_mosquitto_free(context->bridge->local_username);
 			context->bridge->local_username = NULL;
 		}
 		if(context->bridge->local_password){
+			_mosquitto_free(context->bridge->local_password);
 			context->bridge->local_password = NULL;
 		}
-		if(context->bridge->local_clientid){
-			context->bridge->local_clientid = NULL;
+		if(context->bridge->remote_clientid){
+			_mosquitto_free(context->bridge->remote_clientid);
+			context->bridge->remote_clientid = NULL;
+		}
+		if(context->bridge->remote_username){
+			_mosquitto_free(context->bridge->remote_username);
+			context->bridge->remote_username = NULL;
+		}
+		if(context->bridge->remote_password){
+			_mosquitto_free(context->bridge->remote_password);
+			context->bridge->remote_password = NULL;
 		}
 	}
 #endif
