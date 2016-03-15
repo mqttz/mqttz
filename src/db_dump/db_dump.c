09ea5cbf2cc5291139de00776c15038ee0410269
Roger A. Light <roger@atchoo.org>
2016-03-15 00:18:40 +0000

    mosquitto_db_dump can now output some client stats.
    This may be useful for finding out which clients are using significant
    amounts of persistence space.

--- src/db_dump/db_dump.c
+++ src/db_dump/db_dump.c
@@ -28,8 +28,37 @@ Contributors:
 #include <memory_mosq.h>
 #include <persist.h>
 
+#define mosquitto__malloc(A) malloc((A))
+#define mosquitto__free(A) free((A))
+#define _mosquitto_malloc(A) malloc((A))
+#define _mosquitto_free(A) free((A))
+#include <uthash.h>
+
+
+struct client_chunk
+{
+	UT_hash_handle hh_id;
+	char *id;
+	int subscriptions;
+	int subscription_size;
+	int messages;
+	long message_size;
+};
+
+struct msg_store_chunk
+{
+	UT_hash_handle hh;
+	dbid_t store_id;
+	uint32_t length;
+};
+
 static uint32_t db_version;
 static int stats = 0;
+static int client_stats = 0;
+static int do_print = 1;
+
+struct client_chunk *clients_by_id = NULL;
+struct msg_store_chunk *msgs_by_id = NULL;
 
 static int db__client_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 {
@@ -37,6 +66,7 @@ static int db__client_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 	char *client_id = NULL;
 	int rc = 0;
 	time_t disconnect_t;
+	struct client_chunk *cc;
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
@@ -52,35 +82,47 @@ static int db__client_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	read_e(db_fd, client_id, slen);
-	if(!stats) printf("\tClient ID: %s\n", client_id);
+	if(do_print) printf("\tClient ID: %s\n", client_id);
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	last_mid = ntohs(i16temp);
-	if(!stats) printf("\tLast MID: %d\n", last_mid);
+	if(do_print) printf("\tLast MID: %d\n", last_mid);
 
 	if(db_version == 2){
 		disconnect_t = time(NULL);
 	}else{
 		read_e(db_fd, &disconnect_t, sizeof(time_t));
-		if(!stats) printf("\tDisconnect time: %ld\n", disconnect_t);
+		if(do_print) printf("\tDisconnect time: %ld\n", disconnect_t);
 	}
 
-	free(client_id);
+	if(client_stats){
+		cc = calloc(1, sizeof(struct client_chunk));
+		if(!cc){
+			errno = ENOMEM;
+			goto error;
+		}
+		cc->id = client_id;
+		HASH_ADD_KEYPTR(hh_id, clients_by_id, cc->id, strlen(cc->id), cc);
+	}else{
+		free(client_id);
+	}
 
 	return rc;
 error:
 	fprintf(stderr, "Error: %s.", strerror(errno));
-	if(db_fd >= 0) fclose(db_fd);
-	if(client_id) free(client_id);
+	if(db_fd) fclose(db_fd);
+	free(client_id);
 	return 1;
 }
 
-static int db__client_msg_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
+static int db__client_msg_chunk_restore(struct mosquitto_db *db, FILE *db_fd, uint32_t length)
 {
 	dbid_t i64temp, store_id;
 	uint16_t i16temp, slen, mid;
 	uint8_t qos, retain, direction, state, dup;
 	char *client_id = NULL;
+	struct client_chunk *cc;
+	struct msg_store_chunk *msc;
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
@@ -96,38 +138,51 @@ static int db__client_msg_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	read_e(db_fd, client_id, slen);
-	if(!stats) printf("\tClient ID: %s\n", client_id);
+	if(do_print) printf("\tClient ID: %s\n", client_id);
 
 	read_e(db_fd, &i64temp, sizeof(dbid_t));
 	store_id = i64temp;
-	if(!stats) printf("\tStore ID: %ld\n", (long )store_id);
+	if(do_print) printf("\tStore ID: %ld\n", (long )store_id);
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	mid = ntohs(i16temp);
-	if(!stats) printf("\tMID: %d\n", mid);
+	if(do_print) printf("\tMID: %d\n", mid);
 
 	read_e(db_fd, &qos, sizeof(uint8_t));
-	if(!stats) printf("\tQoS: %d\n", qos);
+	if(do_print) printf("\tQoS: %d\n", qos);
 	read_e(db_fd, &retain, sizeof(uint8_t));
-	if(!stats) printf("\tRetain: %d\n", retain);
+	if(do_print) printf("\tRetain: %d\n", retain);
 	read_e(db_fd, &direction, sizeof(uint8_t));
-	if(!stats) printf("\tDirection: %d\n", direction);
+	if(do_print) printf("\tDirection: %d\n", direction);
 	read_e(db_fd, &state, sizeof(uint8_t));
-	if(!stats) printf("\tState: %d\n", state);
+	if(do_print) printf("\tState: %d\n", state);
 	read_e(db_fd, &dup, sizeof(uint8_t));
-	if(!stats) printf("\tDup: %d\n", dup);
+	if(do_print) printf("\tDup: %d\n", dup);
+
+	if(client_stats){
+		HASH_FIND(hh_id, clients_by_id, client_id, strlen(client_id), cc);
+		if(cc){
+			cc->messages++;
+			cc->message_size += length;
+
+			HASH_FIND(hh, msgs_by_id, &store_id, sizeof(dbid_t), msc);
+			if(msc){
+				cc->message_size += msc->length;
+			}
+		}
+	}
 
 	free(client_id);
 
 	return 0;
 error:
 	fprintf(stderr, "Error: %s.", strerror(errno));
-	if(db_fd >= 0) fclose(db_fd);
-	if(client_id) free(client_id);
+	if(db_fd) fclose(db_fd);
+	free(client_id);
 	return 1;
 }
 
-static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
+static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd, uint32_t length)
 {
 	dbid_t i64temp, store_id;
 	uint32_t i32temp, payloadlen;
@@ -138,10 +193,11 @@ static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 	int rc = 0;
 	bool binary;
 	int i;
+	struct msg_store_chunk *mcs;
 
 	read_e(db_fd, &i64temp, sizeof(dbid_t));
 	store_id = i64temp;
-	if(!stats) printf("\tStore ID: %ld\n", (long)store_id);
+	if(do_print) printf("\tStore ID: %ld\n", (long)store_id);
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
@@ -158,16 +214,16 @@ static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 			free(source_id);
 			return 1;
 		}
-		if(!stats) printf("\tSource ID: %s\n", source_id);
+		if(do_print) printf("\tSource ID: %s\n", source_id);
 		free(source_id);
 	}
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	source_mid = ntohs(i16temp);
-	if(!stats) printf("\tSource MID: %d\n", source_mid);
+	if(do_print) printf("\tSource MID: %d\n", source_mid);
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	mid = ntohs(i16temp);
-	if(!stats) printf("\tMID: %d\n", mid);
+	if(do_print) printf("\tMID: %d\n", mid);
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
@@ -186,7 +242,7 @@ static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 			free(topic);
 			return 1;
 		}
-		if(!stats) printf("\tTopic: %s\n", topic);
+		if(do_print) printf("\tTopic: %s\n", topic);
 		free(topic);
 	}else{
 		fprintf(stderr, "Error: Invalid msg_store chunk when restoring persistent database.");
@@ -195,13 +251,13 @@ static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	read_e(db_fd, &qos, sizeof(uint8_t));
-	if(!stats) printf("\tQoS: %d\n", qos);
+	if(do_print) printf("\tQoS: %d\n", qos);
 	read_e(db_fd, &retain, sizeof(uint8_t));
-	if(!stats) printf("\tRetain: %d\n", retain);
+	if(do_print) printf("\tRetain: %d\n", retain);
 	
 	read_e(db_fd, &i32temp, sizeof(uint32_t));
 	payloadlen = ntohl(i32temp);
-	if(!stats) printf("\tPayload Length: %d\n", payloadlen);
+	if(do_print) printf("\tPayload Length: %d\n", payloadlen);
 
 	if(payloadlen){
 		payload = malloc(payloadlen+1);
@@ -226,17 +282,28 @@ static int db__msg_store_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 			if(payload[i] == 0) binary = true;
 		}
 		if(binary == false && payloadlen<256){
-			if(!stats) printf("\tPayload: %s\n", payload);
+			if(do_print) printf("\tPayload: %s\n", payload);
 		}
 		free(payload);
+	}
+
+	if(client_stats){
+		mcs = calloc(1, sizeof(struct msg_store_chunk));
+		if(!mcs){
+			errno = ENOMEM;
+			goto error;
+		}
+		mcs->store_id = store_id;
+		mcs->length = length;
+    	HASH_ADD(hh, msgs_by_id, store_id, sizeof(dbid_t), mcs);
 	}
 
 	return rc;
 error:
 	fprintf(stderr, "Error: %s.", strerror(errno));
-	if(db_fd >= 0) fclose(db_fd);
-	if(source_id) free(source_id);
-	if(topic) free(topic);
+	if(db_fd) fclose(db_fd);
+	free(source_id);
+	free(topic);
 	return 1;
 }
 
@@ -250,17 +317,18 @@ static int db__retain_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	store_id = i64temp;
-	if(!stats) printf("\tStore ID: %ld\n", (long int)store_id);
+	if(do_print) printf("\tStore ID: %ld\n", (long int)store_id);
 	return 0;
 }
 
-static int db__sub_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
+static int db__sub_chunk_restore(struct mosquitto_db *db, FILE *db_fd, uint32_t length)
 {
 	uint16_t i16temp, slen;
 	uint8_t qos;
 	char *client_id;
 	char *topic;
 	int rc = 0;
+	struct client_chunk *cc;
 
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
@@ -271,7 +339,7 @@ static int db__sub_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	read_e(db_fd, client_id, slen);
-	if(!stats) printf("\tClient ID: %s\n", client_id);
+	if(do_print) printf("\tClient ID: %s\n", client_id);
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
 	slen = ntohs(i16temp);
 	topic = calloc(slen+1, sizeof(char));
@@ -282,9 +350,18 @@ static int db__sub_chunk_restore(struct mosquitto_db *db, FILE *db_fd)
 		return 1;
 	}
 	read_e(db_fd, topic, slen);
-	if(!stats) printf("\tTopic: %s\n", topic);
+	if(do_print) printf("\tTopic: %s\n", topic);
 	read_e(db_fd, &qos, sizeof(uint8_t));
-	if(!stats) printf("\tQoS: %d\n", qos);
+	if(do_print) printf("\tQoS: %d\n", qos);
+
+	if(client_stats){
+		HASH_FIND(hh_id, clients_by_id, client_id, strlen(client_id), cc);
+		if(cc){
+			cc->subscriptions++;
+			cc->subscription_size += length;
+		}
+	}
+
 	free(client_id);
 	free(topic);
 
@@ -314,14 +391,20 @@ int main(int argc, char *argv[])
 	long retain_count = 0;
 	long sub_count = 0;
 	long client_count = 0;
+	struct client_chunk *cc, *cc_tmp;
 
 	if(argc == 2){
 		filename = argv[1];
 	}else if(argc == 3 && !strcmp(argv[1], "--stats")){
 		stats = 1;
+		do_print = 0;
+		filename = argv[2];
+	}else if(argc == 3 && !strcmp(argv[1], "--client-stats")){
+		client_stats = 1;
+		do_print = 0;
 		filename = argv[2];
 	}else{
-		fprintf(stderr, "Usage: db_dump [--stats] <mosquitto db filename>\n");
+		fprintf(stderr, "Usage: db_dump [--stats | --client-stats] <mosquitto db filename>\n");
 		return 1;
 	}
 	memset(&db, 0, sizeof(struct mosquitto_db));
@@ -329,13 +412,13 @@ int main(int argc, char *argv[])
 	if(!fd) return 0;
 	read_e(fd, &header, 15);
 	if(!memcmp(header, magic, 15)){
-		if(!stats) printf("Mosquitto DB dump\n");
+		if(do_print) printf("Mosquitto DB dump\n");
 		// Restore DB as normal
 		read_e(fd, &crc, sizeof(uint32_t));
-		if(!stats) printf("CRC: %d\n", crc);
+		if(do_print) printf("CRC: %d\n", crc);
 		read_e(fd, &i32temp, sizeof(uint32_t));
 		db_version = ntohl(i32temp);
-		if(!stats) printf("DB version: %d\n", db_version);
+		if(do_print) printf("DB version: %d\n", db_version);
 
 		while(rlen = fread(&i16temp, sizeof(uint16_t), 1, fd), rlen == 1){
 			chunk = ntohs(i16temp);
@@ -344,12 +427,12 @@ int main(int argc, char *argv[])
 			switch(chunk){
 				case DB_CHUNK_CFG:
 					cfg_count++;
-					if(!stats) printf("DB_CHUNK_CFG:\n");
-					if(!stats) printf("\tLength: %d\n", length);
+					if(do_print) printf("DB_CHUNK_CFG:\n");
+					if(do_print) printf("\tLength: %d\n", length);
 					read_e(fd, &i8temp, sizeof(uint8_t)); // shutdown
-					if(!stats) printf("\tShutdown: %d\n", i8temp);
+					if(do_print) printf("\tShutdown: %d\n", i8temp);
 					read_e(fd, &i8temp, sizeof(uint8_t)); // sizeof(dbid_t)
-					if(!stats) printf("\tDB ID size: %d\n", i8temp);
+					if(do_print) printf("\tDB ID size: %d\n", i8temp);
 					if(i8temp != sizeof(dbid_t)){
 						fprintf(stderr, "Error: Incompatible database configuration (dbid size is %d bytes, expected %ld)",
 								i8temp, sizeof(dbid_t));
@@ -357,41 +440,41 @@ int main(int argc, char *argv[])
 						return 1;
 					}
 					read_e(fd, &i64temp, sizeof(dbid_t));
-					if(!stats) printf("\tLast DB ID: %ld\n", (long)i64temp);
+					if(do_print) printf("\tLast DB ID: %ld\n", (long)i64temp);
 					break;
 
 				case DB_CHUNK_MSG_STORE:
 					msg_store_count++;
-					if(!stats) printf("DB_CHUNK_MSG_STORE:\n");
-					if(!stats) printf("\tLength: %d\n", length);
-					if(db__msg_store_chunk_restore(&db, fd)) return 1;
+					if(do_print) printf("DB_CHUNK_MSG_STORE:\n");
+					if(do_print) printf("\tLength: %d\n", length);
+					if(db__msg_store_chunk_restore(&db, fd, length)) return 1;
 					break;
 
 				case DB_CHUNK_CLIENT_MSG:
 					client_msg_count++;
-					if(!stats) printf("DB_CHUNK_CLIENT_MSG:\n");
-					if(!stats) printf("\tLength: %d\n", length);
-					if(db__client_msg_chunk_restore(&db, fd)) return 1;
+					if(do_print) printf("DB_CHUNK_CLIENT_MSG:\n");
+					if(do_print) printf("\tLength: %d\n", length);
+					if(db__client_msg_chunk_restore(&db, fd, length)) return 1;
 					break;
 
 				case DB_CHUNK_RETAIN:
 					retain_count++;
-					if(!stats) printf("DB_CHUNK_RETAIN:\n");
-					if(!stats) printf("\tLength: %d\n", length);
+					if(do_print) printf("DB_CHUNK_RETAIN:\n");
+					if(do_print) printf("\tLength: %d\n", length);
 					if(db__retain_chunk_restore(&db, fd)) return 1;
 					break;
 
 				case DB_CHUNK_SUB:
 					sub_count++;
-					if(!stats) printf("DB_CHUNK_SUB:\n");
-					if(!stats) printf("\tLength: %d\n", length);
-					if(db__sub_chunk_restore(&db, fd)) return 1;
+					if(do_print) printf("DB_CHUNK_SUB:\n");
+					if(do_print) printf("\tLength: %d\n", length);
+					if(db__sub_chunk_restore(&db, fd, length)) return 1;
 					break;
 
 				case DB_CHUNK_CLIENT:
 					client_count++;
-					if(!stats) printf("DB_CHUNK_CLIENT:\n");
-					if(!stats) printf("\tLength: %d\n", length);
+					if(do_print) printf("DB_CHUNK_CLIENT:\n");
+					if(do_print) printf("\tLength: %d\n", length);
 					if(db__client_chunk_restore(&db, fd)) return 1;
 					break;
 
@@ -417,6 +500,14 @@ int main(int argc, char *argv[])
 		printf("DB_CHUNK_SUB:        %ld\n", sub_count);
 		printf("DB_CHUNK_CLIENT:     %ld\n", client_count);
 	}
+
+	if(client_stats){
+		HASH_ITER(hh_id, clients_by_id, cc, cc_tmp){
+			printf("SC: %d SS: %d MC: %d MS: %ld   ", cc->subscriptions, cc->subscription_size, cc->messages, cc->message_size);
+			printf("%s\n", cc->id);
+		}
+	}
+
 	return rc;
 error:
 	fprintf(stderr, "Error: %s.", strerror(errno));
