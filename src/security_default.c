cd17ca45cd313dc00480091505f708858db73ee9
Roger A. Light <roger@atchoo.org>
2017-06-27 15:10:43 +0100

    [462] Relax CVE-2017-7650 checks.
    Checks for '/' are no longer made, this character is a much lower risk
    and is widely used in usernames.
    
    Bug: https://github.com/eclipse/mosquitto/issues/462

--- src/security_default.c
+++ src/security_default.c
@@ -264,18 +264,18 @@ int mosquitto_acl_check_default(struct mosquitto_db *db, struct mosquitto *conte
 
 	if(acl_root){
 		/* We are using pattern based acls. Check whether the username or
-		 * client id contains a +, # or / and if so deny access.
+		 * client id contains a + or # and if so deny access.
 		 *
 		 * Without this, a malicious client may configure its username/client
 		 * id to bypass ACL checks (or have a username/client id that cannot
 		 * publish or receive messages to its own place in the hierarchy).
 		 */
-		if(context->username && strpbrk(context->username, "+#/")){
+		if(context->username && strpbrk(context->username, "+#")){
 			_mosquitto_log_printf(NULL, MOSQ_LOG_NOTICE, "ACL denying access to client with dangerous username \"%s\"", context->username);
 			return MOSQ_ERR_ACL_DENIED;
 		}
 
-		if(context->id && strpbrk(context->id, "+#/")){
+		if(context->id && strpbrk(context->id, "+#")){
 			_mosquitto_log_printf(NULL, MOSQ_LOG_NOTICE, "ACL denying access to client with dangerous client id \"%s\"", context->id);
 			return MOSQ_ERR_ACL_DENIED;
 		}
