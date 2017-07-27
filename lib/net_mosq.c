124ee1af914c8d8f7f5b28863d0fd6908247c7f5
Roger A. Light <roger@atchoo.org>
2017-07-27 14:56:10 +0100

    [490] Further fix for auth related crashes.
    Bug: https://github.com/eclipse/mosquitto/issues/490

--- lib/net_mosq.c
+++ lib/net_mosq.c
@@ -230,22 +230,23 @@ int _mosquitto_socket_close(struct mosquitto *mosq)
 	}
 #endif
 
-	if((int)mosq->sock >= 0){
-#ifdef WITH_BROKER
-		HASH_DELETE(hh_sock, db->contexts_by_sock, mosq);
-#endif
-		rc = COMPAT_CLOSE(mosq->sock);
-		mosq->sock = INVALID_SOCKET;
 #ifdef WITH_WEBSOCKETS
-	}else if(mosq->sock == WEBSOCKET_CLIENT){
+	if(mosq->wsi)
+	{
 		if(mosq->state != mosq_cs_disconnecting){
 			mosq->state = mosq_cs_disconnect_ws;
 		}
-		if(mosq->wsi){
-			libwebsocket_callback_on_writable(mosq->ws_context, mosq->wsi);
-		}
-		mosq->sock = INVALID_SOCKET;
+		libwebsocket_callback_on_writable(mosq->ws_context, mosq->wsi);
+	}else
 #endif
+	{
+		if((int)mosq->sock >= 0){
+#ifdef WITH_BROKER
+			HASH_DELETE(hh_sock, db->contexts_by_sock, mosq);
+#endif
+			rc = COMPAT_CLOSE(mosq->sock);
+			mosq->sock = INVALID_SOCKET;
+		}
 	}
 
 #ifdef WITH_BROKER
