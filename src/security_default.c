ecb4006f388c18bbc997d9e3ac45fe8f52775e35
Roger A. Light <roger@atchoo.org>
2018-08-09 14:32:02 +0100

    Don't reject ACL patterns without %u or %c, just warn.
    Using `pattern blah/#` is a legitimate method of getting a global ACL.
    We shouldn't be changing behaviour in a fixes release.

--- src/security_default.c
+++ src/security_default.c
@@ -280,11 +280,9 @@ int add__acl_pattern(struct mosquitto__security_options *security_opts, const ch
 	}
 
 	if(acl->ccount == 0 && acl->ucount == 0){
-		log__printf(NULL, MOSQ_LOG_ERR,
-				"Error: ACL pattern '%s' does not contain '%%c' or '%%u'.",
+		log__printf(NULL, MOSQ_LOG_WARNING,
+				"Warning: ACL pattern '%s' does not contain '%%c' or '%%u'.",
 				topic);
-
-		return MOSQ_ERR_INVAL;
 	}
 
 	if(security_opts->acl_patterns){
