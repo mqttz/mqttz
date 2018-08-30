a127c93a5ae804b096af468d4f18658997794904
Roger A. Light <roger@atchoo.org>
2018-08-30 18:55:43 +0100

    Property identifiers.

--- lib/mqtt_protocol.h
+++ lib/mqtt_protocol.h
@@ -104,6 +104,36 @@ enum mqtt5_return_codes {
 	RC5_WILDCARD_SUBS_NOT_SUPPORTED = 162,	/* SUBACK, DISCONNECT */
 };
 
+enum mqtt5_property {
+	PROP_PAYLOAD_FORMAT_INDICATOR = 1,		/* Byte :				PUBLISH, Will Properties */
+	PROP_MESSAGE_EXPIRY_INTERVAL = 2,		/* 4 byte int :			PUBLISH, Will Properties */
+	PROP_CONTENT_TYPE = 3,					/* UTF-8 string :		PUBLISH, Will Properties */
+	PROP_RESPONSE_TOPIC = 8,				/* UTF-8 string :		PUBLISH, Will Properties */
+	PROP_CORRELATION_DATA = 9,				/* Binary Data :		PUBLISH, Will Properties */
+	PROP_SUBSCRIPTION_IDENTIFIER = 11,		/* Variable byte int :	PUBLISH, SUBSCRIBE */
+	PROP_SESSION_EXPIRY_INTERVAL = 17,		/* 4 byte int :			CONNECT, CONNACK, DISCONNECT */
+	PROP_ASSIGNED_CLIENT_IDENTIFIER = 18,	/* UTF-8 string :		CONNACK */
+	PROP_SERVER_KEEP_ALIVE = 19,			/* 2 byte int :			CONNACK */
+	PROP_AUTHENTICATION_METHOD = 21,		/* UTF-8 string :		CONNECT, CONNACK, AUTH */
+	PROP_AUTHENTICATION_DATA = 22,			/* Binary Data :		CONNECT, CONNACK, AUTH */
+	PROP_REQUEST_PROBLEM_INFO = 23,			/* Byte :				CONNECT */
+	PROP_WILL_DELAY_INTERVAL = 24,			/* 4 byte int :			Will properties */
+	PROP_REQUEST_RESPONSE_INFO = 25,		/* Byte :				CONNECT */
+	PROP_RESPONSE_INFO = 26,				/* UTF-8 string :		CONNACK */
+	PROP_SERVER_REFERENCE = 28,				/* UTF-8 string :		CONNACK, DISCONNECT */
+	PROP_REASON_STRING = 31,				/* UTF-8 string :		All except Will properties */
+	PROP_RECEIVE_MAXIMUM = 33,				/* 2 byte int :			CONNECT, CONNACK */
+	PROP_TOPIC_ALIAS_MAXIMUM = 34,			/* 2 byte int :			CONNECT, CONNACK */
+	PROP_TOPIC_ALIAS = 35,					/* 2 byte int :			PUBLISH */
+	PROP_MAXIMUM_QOS = 36,					/* Byte :				CONNACK */
+	PROP_RETAIN_AVAILABLE = 37,				/* Byte :				CONNACK */
+	PROP_USER_PROPERTY = 38,				/* UTF-8 string pair :	All */
+	PROP_MAXIMUM_PACKET_SIZE = 39,			/* 4 byte int :			CONNECT, CONNACK */
+	PROP_WILDCARD_SUB_AVAILABLE = 40,		/* Byte :				CONNACK */
+	PROP_SUBSCRIPTION_ID_AVAILABLE = 41,	/* Byte :				CONNACK */
+	PROP_SHARED_SUB_AVAILABLE = 42,			/* Byte :				CONNACK */
+};
+
 #define MQTT_MAX_PAYLOAD 268435455
 
 #endif
