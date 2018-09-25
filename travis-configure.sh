9200a8e98b5b3f28b8b7dfabba65cb2bff464e34
Roger A. Light <roger@atchoo.org>
2018-09-25 14:25:02 +0100

    Better attempt at fixing mac travis.

--- travis-configure.sh
+++ travis-configure.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-	export CFLAGS="-I/usr/local/opt/openssl/include $CFLAGS"
-	export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
-	cmake .
+	cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl .
 fi
