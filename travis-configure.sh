e3bfb1795e21c5eed60fa90035b3e41b1bbab252
Roger A. Light <roger@atchoo.org>
2018-09-25 12:11:03 +0100

    Attempt to fix Mac build on travis.

--- travis-configure.sh
+++ travis-configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	export CFLAGS="-I/usr/local/opt/openssl/include $CFLAGS"
+	export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
 	cmake .
 fi
