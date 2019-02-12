e72d1d6ff5ba2ee6b43273c52c7c9616d73d6726
Roger A. Light <roger@atchoo.org>
2019-02-12 11:26:23 +0000

    Fix `mosquitto_topic_matches_sub()` rc with sub=="topic/#abc".
    This now returns MOSQ_ERR_INVAL as expected.

--- lib/util_mosq.c
+++ lib/util_mosq.c
@@ -245,6 +245,7 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
 {
 	int spos, tpos;
 	bool multilevel_wildcard = false;
+	int i;
 
 	if(!result) return MOSQ_ERR_INVAL;
 	*result = false;
@@ -270,7 +271,7 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
 	tpos = 0;
 
 	while(spos < sublen && tpos <= topiclen){
-		if(sub[spos] == topic[tpos]){
+		if(tpos == topiclen || sub[spos] == topic[tpos]){
 			if(tpos == topiclen-1){
 				/* Check for e.g. foo matching foo/# */
 				if(spos == sublen-3
@@ -335,6 +336,12 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
 					*result = true;
 					multilevel_wildcard = true;
 					return MOSQ_ERR_SUCCESS;
+				}
+
+				for(i=spos; i<sublen; i++){
+					if(sub[i] == '#' && i+1 != sublen){
+						return MOSQ_ERR_INVAL;
+					}
 				}
 				return MOSQ_ERR_SUCCESS;
 			}
