d5dfd575b3d47f0216056fd1aa05bfd8066b6d9a
Roger A. Light <roger@atchoo.org>
2019-02-13 11:51:52 +0000

    Fix broker TLS tests.

--- test/broker/08-ssl-connect-cert-auth-without.py
+++ test/broker/08-ssl-connect-cert-auth-without.py
@@ -45,6 +45,7 @@ ssock = ssl.wrap_socket(sock, ca_certs="../ssl/test-root-ca.crt", cert_reqs=ssl.
 ssock.settimeout(20)
 try:
     ssock.connect(("localhost", port1))
+    mosq_test.do_send_receive(ssock, connect_packet, "", "connack")
 except ssl.SSLError as err:
     if err.errno == 1:
         rc = 0
