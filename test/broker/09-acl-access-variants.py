0d7523c3093fe7394853505213ddc6f57c679fe4
Roger A. Light <roger@atchoo.org>
2019-03-28 17:07:42 +0000

    Use helpers in all tests.

--- test/broker/09-acl-access-variants.py
+++ test/broker/09-acl-access-variants.py
@@ -2,15 +2,7 @@
 
 # Check access
 
-
-import inspect, os, sys
-# From http://stackoverflow.com/questions/279237/python-import-a-module-from-a-folder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-import mosq_test
-import signal
+from mosq_test_helper import *
 
 def write_config(filename, port, per_listener):
     with open(filename, 'w') as f:
